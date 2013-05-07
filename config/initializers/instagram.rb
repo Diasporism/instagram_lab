32d6020966d98a9149ed65ec3861dd0444dc124b
Logan Sears <lsears322@gmail.com>
2013-05-07 10:38:26 -0600

    initialized app

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,4 @@
+Instagram.configure do |config|
+  config.client_id = YOUR_CLIENT_KEY
+  config.access_token = YOUR_ACCESS_TOKEN
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstagramLab::Application.config.secret_token = '0b25cb1bb9c6a79607644498581ab27d5b4c001e1c59c2445d0994c9cc0f973a1834d9e63d9264680c51250bd210b8467cf1780ae82f665b69e2442b27ed1966'
